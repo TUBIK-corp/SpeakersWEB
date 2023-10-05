6f253c2e08a4599378466748ace23a97e7a9d93d
Vasys <Vasys@K1-IT-004>
2023-10-05 18:12:07 +0300

    Ok2

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpeakersWEB.Controllers
+{
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok(new FuckOff() { Who="ur mom"});
+        }
+        public class FuckOff
+        {
+            public string Who { get; set; }
+        }
+    }
+}
