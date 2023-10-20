6806ff804e31cf2656ad8bac28586b7ac186849a
Vasys <Vasys@K1-IT-004>
2023-10-20 12:30:32 +0300

    OKAY2

---
+++ Services/AuthorizationService.cs
@@ -0,0 +1,30 @@
+﻿using SpeakersWEB.Models;
+
+namespace SpeakersWEB.Services
+{
+    public class AuthorizationService
+    {
+        private readonly BellsContext _context;
+
+        public AuthorizationService(BellsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserToken> GenerateTokenAsync(User user, int tokenLifeTime)
+        {
+            var token = new UserToken
+            {
+                Token = Guid.NewGuid().ToString()
+            };
+
+            await _context.UserTokens.AddAsync(token);
+
+            token.UserId = user.Id;
+
+            await _context.SaveChangesAsync();
+
+            return token;
+        }
+    }
+}
